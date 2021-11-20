c065fe8cb6d83b028d5403d5388dcd0cb814a13c
SantiagoFunes <santiagofunes.97@hotmail.com>
2021-11-20 00:01:30 -0300

    Actualizacion

--- app.js
+++ app.js
@@ -2,14 +2,38 @@ const express = require("express")
 const app = express()
 const path = require("path");
 const exp = require("constants");
+app.use(express.urlencoded({extended: true}))
 
 const publicpath = path.resolve(__dirname,'./public');
 app.use(express.static(publicpath))
-app.listen(3000, ()=> {
-    console.log("Servidor corriendo en el puerto 3000");
-})
+
 
 app.get("/",(req,res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 })
 
+app.get("/register",(req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
+
+app.get("/login",(req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
+
+app.post('/register', (req,res)=>{
+    console.log(req.body);
+    res.sendFile(__dirname + '/');
+    res.redirect("/")
+});
+
+app.post('/login', (req,res)=>{
+    console.log(req.body);
+    res.sendFile(__dirname + '/');
+    res.redirect("/")
+});
+
+
+
+app.listen(3000, ()=> {
+    console.log("Servidor corriendo en el puerto 3000");
+})
