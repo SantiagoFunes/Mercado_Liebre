ef81f20a21f64baeb8be9b32807b25fad2662b33
SantiagoFunes <santiagofunes.97@hotmail.com>
2021-11-22 22:06:11 -0300

    procfile

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.post('/login', (req,res)=>{
 
 
 
-app.listen(3000, ()=> {
-    console.log("Servidor corriendo en el puerto 3000");
-})
+app.listen(process.env.PORT || 4000, ()=>{
+    console.log('Servidor funcionando');
+});
